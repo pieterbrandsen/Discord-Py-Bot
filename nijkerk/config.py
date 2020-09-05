0ef160d361d5f063d21160bb6d3c554a62e2f1ef
Pieter <pieterbrandsen2@gmail.com>
2020-09-05 13:41:52 +0200

    Update config.py

--- nijkerk/config.py
+++ nijkerk/config.py
@@ -2,7 +2,7 @@
 # Real token
 #token = 'NzI1NjQwNTc3OTk1MTEyNDg4.XvWRmw.W8xiIwh_tibizlGwwbBNpJGJnY0'
 # Test token
-token = 'NzI1NzY0NDQxOTE5OTc5NjIw.XvYXGw.TJQ4_5n70ftgRHnCaVWqnkD0Rc4'
+token = 'NzUwMDM3MjE2MTE3MzkxMzgw.X00slw.0Qxxjvg3rxqPVUqnGus5GkUh5kA'
 
 prefix = '!'  # put prefix here
 
